5128bd964a06682817783835c164e4113cae04d6
Hajin_Kim <kimhajin@yonsei.ac.kr>
2022-07-24 20:13:20 +0900

    Update UserArgumentResolver.kt

--- src/main/kotlin/com/web/application/resolver/UserArgumentResolver.kt
+++ src/main/kotlin/com/web/application/resolver/UserArgumentResolver.kt
@@ -37,62 +37,65 @@ class UserArgumentResolver(
     ): CommunityUser? {
         val session: HttpSession =
             (RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes).request.session
-        val communityUser = session.getAttribute("user") as CommunityUser?
-        return getUser(communityUser, session)
+        return session.getAttribute("user")
+            ?.let { it as CommunityUser } // 세션 attribute 에 "user"가 있을 경우 그대로 반환
+            ?: runCatching { // 세션에 없을 경우
+                // 인증 정보를 가져옴 (currently authenticated principal, or an authentication request token)
+                val authentication: OAuth2AuthenticationToken =
+                    SecurityContextHolder.getContext().authentication as OAuth2AuthenticationToken
+                val map: Map<String, Any> = authentication.principal.attributes
+
+                // 인증 정보로부터 CommunityUser 임시 생성 (DAO)
+                val convertUser = createTempUserOrNull(authentication.authorizedClientRegistrationId, map)!!
+
+                // CommunityUser 조회해보고 없으면 저장
+                val communityUser = communityUserRepository.findByEmail(convertUser.email)
+                    ?: communityUserRepository.save(convertUser)
+                setRoleIfNotSame(communityUser, authentication, map) // 권한 동기화
+                session.setAttribute("user", communityUser) // 세션 attribute 에 추가
+
+                communityUser
+            }.getOrNull()
     }
 
-    private fun getUser(communityUser: CommunityUser?, session: HttpSession): CommunityUser? {
-        var communityUser = communityUser
-        return communityUser ?: try {
-            val authentication: OAuth2AuthenticationToken =
-                SecurityContextHolder.getContext().authentication as OAuth2AuthenticationToken
-            val map: Map<String, Any> = authentication.principal.attributes
-            val convertUser = convertUser(authentication.authorizedClientRegistrationId, map)!!
-            communityUser = communityUserRepository.findByEmail(convertUser.email)
-            if (communityUser == null) {
-                communityUser = communityUserRepository.save(convertUser)
+    private fun createTempUserOrNull(authority: String, map: Map<String, Any>): CommunityUser? {
+        val socialType = SocialType.values().firstOrNull { it.type == authority }
+
+        val name: String
+        val password: String? = null
+        val email: String
+        val principal: String
+
+        when (socialType) {
+            SocialType.FACEBOOK -> {
+                name = map["name"].toString()
+                email = map["email"].toString()
+                principal = map["id"].toString()
             }
-            setRoleIfNotSame(communityUser, authentication, map)
-            session.setAttribute("user", communityUser)
 
-            communityUser
-        } catch (e: ClassCastException) {
-            communityUser
-        }
-    }
+            SocialType.GOOGLE -> {
+                name = map["name"].toString()
+                email = map["email"].toString()
+                principal = map["sub"].toString()
+            }
 
-    private fun convertUser(authority: String, map: Map<String, Any>): CommunityUser? =
-        when (authority) {
-            SocialType.FACEBOOK.value -> getModernUser(
-                SocialType.FACEBOOK,
-                map,
-            )
-            SocialType.GOOGLE.value -> getModernUser(
-                SocialType.GOOGLE,
-                map,
-            )
-            SocialType.KAKAO.value -> getKaKaoUser(map)
-            else -> null
+            SocialType.KAKAO -> {
+                val kakaoAccount: Map<String, Any> = map["kakao_account"] as Map<String, Any>
+                val profile: Map<String, String> = kakaoAccount["profile"] as Map<String, String>
+                name = profile["nickname"]!!
+                email = kakaoAccount["email"].toString()
+                principal = map["id"].toString()
+            }
+
+            else -> return null
         }
 
-    private fun getModernUser(socialType: SocialType, map: Map<String, Any>): CommunityUser {
         return CommunityUser(
-            name = map["name"].toString(),
-            password = null,
-            email = map["email"].toString(),
-            principal = map["id"].toString(),
+            name = name,
+            password = password,
+            email = email,
+            principal = principal,
             socialType = socialType,
-        )
-    }
-
-    private fun getKaKaoUser(map: Map<String, Any>): CommunityUser {
-        val propertyMap: Map<String, String> = map["properties"] as HashMap<String, String>
-        return CommunityUser(
-            name = propertyMap["nickname"]!!,
-            password = null,
-            email = map["kaccount_email"].toString(),
-            principal = map["id"].toString(),
-            socialType = SocialType.KAKAO,
         )
     }
 
